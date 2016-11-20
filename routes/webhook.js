a1521cd33a9927f90896e9ade8f37cc6ceda9687
TheCreatorFr <MrBrondinNicolas@gmail.com>
2016-11-20 11:01:54 +0100

    [FIX] Response not working on api.ai

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,12 +1,21 @@
 exports.process_request = function(req, res) {
     var o = {
-        "fulfillment": {
+        
             "speech": "Today in Boston: Fair, the temperature is 37 F",
             "source": "apiai-weather-webhook-sample",
             "displayText": "Today in Boston: Fair, the temperature is 37 F"
-        }
+        
     };
     res.json(200,o);
     
 };
-
+/*
+{
+    product: "computer"
+    activity: "pro",
+    power: "average",
+    storage: "much storage",
+         screen: '13"'
+        price: "1300",
+   */        
+        
