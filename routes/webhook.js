fb705973ea5b1401ecb86b7e189e7fd5313e33a7
TheCreatorFr <MrBrondinNicolas@gmail.com>
2016-11-20 12:03:14 +0100

    [WORKING] shit working

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,10 +1,10 @@
 exports.process_request = function(req, res) {
     var o = {
-        
+        "fulfillment": {
             "speech": "Today in Boston: Fair, the temperature is 37 F",
             "source": "apiai-weather-webhook-sample",
             "displayText": "Today in Boston: Fair, the temperature is 37 F"
-        
+        }
     };
     res.json(200,o);
     
